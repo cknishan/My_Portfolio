f510825e6809d1743868e4ead8eea2ad5071f88b
Nishan <nishan.chakma.nish@gmail.com>
2022-12-16 11:04:02 +1100

    nav bar

---
+++ js/responsiveNav.js
@@ -0,0 +1,24 @@
+"use strict"
+
+const modeBtn = document.querySelector('#btn-mode');
+const nav = document.querySelector('#nav-bar');
+const menuListItems = document.querySelector(".menu-content");
+const menuBtn = document.querySelector(".menu-btn")
+const navContent = document.querySelector(".menu-content")
+const navLinks = document.querySelectorAll(".nav-link")
+
+// Responsive Nav Bar
+menuBtn.addEventListener("click",() => {
+    navToggle();
+  })
+  
+  function navToggle(){
+    menuBtn.classList.toggle("active");
+    navContent.classList.toggle('active')
+  }
+  
+  navLinks.forEach( item =>{
+    item.addEventListener("click", () => {
+      navToggle()
+    })
+  })
